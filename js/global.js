c78383d4a4eac626f037c9c054de83eb7bfe6d75
AhmadNuzulurRozaq <nuzulnazil14@gmail.com>
2026-01-22 22:37:53 +0700

    Update Responsive

--- js/global.js
+++ js/global.js
@@ -2,5 +2,6 @@ const humberger = document.getElementById("humbergerMenu");
 const navbar =  document.getElementById("navbarMenu");
 
 humberger.addEventListener('click', ()=>{
+    humberger.classList.toggle('active');
     navbar.classList.toggle('active');
 });
